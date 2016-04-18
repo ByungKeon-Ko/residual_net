209df03cf8bb9b7004e3ed422b2e1268f956f5d3
Myungsub Choi <cms6539@gmail.com>
2016-04-18 14:21:16 +0900

    Save res-56 graph jpeg

--- matlab/plot_acc_graph.m
+++ matlab/plot_acc_graph.m
@@ -1,11 +1,11 @@
 
-load '../output_data/train_acc_plain_20layer.txt';
-load '../output_data/test_acc_plain_20layer.txt';
+load '../output_data/train_acc_plain_56layer.txt';
+load '../output_data/test_acc_plain_56layer.txt';
 
-plain_x1 = train_acc_plain_20layer(:,1)/1e4;
-plain_y1 = train_acc_plain_20layer(:,2);
-plain_x2 = test_acc_plain_20layer(:,1)/1e4;
-plain_y2 = test_acc_plain_20layer(:,2);
+plain_x1 = train_acc_plain_56layer(:,1)/1e4;
+plain_y1 = train_acc_plain_56layer(:,2);
+plain_x2 = test_acc_plain_56layer(:,1)/1e4;
+plain_y2 = test_acc_plain_56layer(:,2);
 
 figure(1);
 hold on;
@@ -14,17 +14,17 @@ plot(plain_x2, plain_y2, 'b-');
 axis( [ 0 7 0 0.2 ] )
 xlabel('iteration (1e4)');
 ylabel('test & train error(ratio)' );
-title('plain 20 layer result' );
+title('plain 56 layer result' );
 grid on;
 hold off;
 
-load '../output_data/train_acc_res_20layer.txt';
-load '../output_data/test_acc_res_20layer.txt';
+load '../output_data/train_acc_res_56layer.txt';
+load '../output_data/test_acc_res_56layer.txt';
 
-res_x1 = train_acc_res_20layer(:,1)/1e4;
-res_y1 = train_acc_res_20layer(:,2);
-res_x2 = test_acc_res_20layer(:,1)/1e4;
-res_y2 = test_acc_res_20layer(:,2);
+res_x1 = train_acc_res_56layer(:,1)/1e4;
+res_y1 = train_acc_res_56layer(:,2);
+res_x2 = test_acc_res_56layer(:,1)/1e4;
+res_y2 = test_acc_res_56layer(:,2);
 
 figure(3);
 hold on;
@@ -33,7 +33,7 @@ plot(res_x2, res_y2, 'b-');
 axis( [ 0 7 0 0.2 ] )
 xlabel('iteration (1e4)');
 ylabel('test & train error(ratio)' );
-title('residual 20 layer result' );
+title('residual 56 layer result' );
 grid on;
 hold off;
 
