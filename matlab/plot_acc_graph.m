bd713c616777211471e10847070beeca8bf0dcf5
Myungsub Choi <cms6539@gmail.com>
2016-04-25 15:06:09 +0900

    update pre_post_164.jpg

--- matlab/plot_acc_graph.m
+++ matlab/plot_acc_graph.m
@@ -55,9 +55,9 @@ res_y4 = test_acc_bottle_164layer(:,2);
 
 figure(3);
 hold on;
-plot(res_x1, res_y1, 'r.');
+plot(res_x1, res_y1, 'r--');
 plot(res_x2, res_y2, 'r-');
-plot(res_x3, res_y3, 'b.');
+plot(res_x3, res_y3, 'b--');
 plot(res_x4, res_y4, 'b-');
 axis( [ 0 7 0 0.2 ] )
 xlabel('iteration (1e4)');
